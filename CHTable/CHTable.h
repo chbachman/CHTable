81ba383b66b04d4124dd0d63643169fc33bbdc48
chbachman <cbachman@bachmangroup.com>
2018-06-08 22:19:04 -0500

    Initial Commit

---
+++ CHTable/CHTable.h
@@ -0,0 +1,19 @@
+//
+//  CHTable.h
+//  CHTable
+//
+//  Created by Chandler on 6/8/18.
+//  Copyright © 2018 chbachman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CHTable.
+FOUNDATION_EXPORT double CHTableVersionNumber;
+
+//! Project version string for CHTable.
+FOUNDATION_EXPORT const unsigned char CHTableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHTable/PublicHeader.h>
+
+
